d6e822e8ff296db65bab398917fd3bf2c0085863
Samuel Pires <samuel.ferpires@gmail.com>
2023-05-23 20:42:10 -0300

    WIP: refactoring tailwind implementation

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,12 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+
+Route::get('/create-post', function () {
+    return view('create-post');
+})->middleware(['auth', 'verified'])->name('create-post');
+
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
