01b527cf5ba092535d0f54119439d0e4611a9977
Samuel Pires <samuel.ferpires@gmail.com>
2023-05-23 20:58:31 -0300

    feat: (UI) working on create post ui implementation

--- resources/views/create-post.blade.php
+++ resources/views/create-post.blade.php
@@ -5,7 +5,11 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        something here
+    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8 text-white flex flex-col gap-4">
+        <div class="flex flex-col gap-2">
+            <label for="content" class="text-white font-bold">Content:</label>
+            <textarea name="content" class="w-full p-2 text-white bg-gray-800 resize-none"></textarea>
+        </div>
+        <button class="bg-gray-600 w-full rounded-md p-3">Create</button>
     </div>
 </x-app-layout>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,9 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('create-post')" :active="request()->routeIs('create-post')">
+                        {{ __('Share new thought') }}
+                    </x-nav-link>
                 </div>
             </div>
 
